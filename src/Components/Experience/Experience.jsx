b68670bfb16bc0058cca0d85143720e2bdd9f165
Tejas Singhal <tjssinghal@icloud.com>
2024-06-24 21:54:16 +0530

    Updated resume and experience descriptions

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import styles from "./Experience.module.css";
-import skills from "../../data/skills.json";
+// import skills from "../../data/skills.json";
 import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
